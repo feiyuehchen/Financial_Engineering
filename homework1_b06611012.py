626541d9d8bc31372f49b01ceb4e2c39a18f150c
feiyuehchen <brian9436et@gmail.com>
2020-03-04 20:52:27 +0800

    create homework1_b06611012.py

---
+++ homework1_b06611012.py
@@ -0,0 +1,16 @@
+import math
+
+#get three value
+
+print("請輸入您的本金（萬元）、期數（年）與年利率（%），且格式為[本金,期數,年利率]")
+a = input().split(",")
+money = a[0]
+year = a[1]
+interest = a[2]
+
+"""
+while True:
+  print("本金：", money, "期年：", year, "年利率：", interest)
+  print("請重新確認一次數字，正確請打yes，失敗請")
+"""
+
